2d00d26b226bae411ecef7973796284c1b75993c
Matthieu Marshall <matthieu.marshall@gmail.com>
2020-04-21 17:51:08 +0100

    Adding download of data to main.py

--- scripts/main.py
+++ scripts/main.py
@@ -1,5 +1,20 @@
 import nltk
 import pandas as pd
 import numpy as np
+import string
 
+data_source_url = "https://raw.githubusercontent.com/kolaveridi/kaggle-Twitter-US-Airline-Sentiment-/master/Tweets.csv"
+airline_tweets = pd.read_csv(data_source_url, sep=",")
 
+is_positive = tweets['airline_sentiment'].str.contains("positive")
+is_negative = tweets['airline_sentiment'].str.contains("negative")
+is_neutral = tweets['airline_sentiment'].str.contains("neutral")
+
+positive_tweets = tweets[is_positive]
+negative_tweets = tweets[is_negative]
+neutral_tweets = tweets[is_neutral]
+
+nltk.download("punkt")
+nltk.download("stopwords")
+
+useless_words = nltk.corpus.stopwords.words("english") + list(string.punctuation)
