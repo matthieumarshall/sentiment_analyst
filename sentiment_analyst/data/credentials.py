031e6277d48c4d571cee2bd56ce5e05fa5d47686
Matthieu Marshall <matthieu.marshall@gmail.com>
2021-01-30 17:09:23 +0000

    Using github Actions secrets in CI workflow and in environment variables

---
+++ sentiment_analyst/data/credentials.py
@@ -0,0 +1,6 @@
+import os
+
+consumer_key = r"cGK8aIfCCElw86PdYGxX85hH2"
+consumer_secret = os.getenv("TWEEPY_CONSUMER_SECRET")
+access_token = r"17760955-CtCzjtYwoS7o6c6eGioCnhWEFKwTVv2IqPV7RmZ8Y"
+access_token_secret = os.getenv("TWEEPY_ACCESS_TOKEN_SECRET")
